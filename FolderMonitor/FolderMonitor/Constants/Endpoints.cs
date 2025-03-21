2b76cbc4ce1a4ac0fb1c8301b64dd0c8b4f58e89
vladislavnikov <vladislav.d.nikov04@gmail.com>
2025-03-21 16:24:07 +0200

    Implement user authentication

---
+++ FolderMonitor/FolderMonitor/Constants/Endpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace FolderMonitor.Constants
+{
+    public  class Endpoints
+    {
+        public static readonly string BASE_URL = "https://testserver.moveitcloud.com";
+        public static readonly string TOKEN = "/api/v1/token";
+    }
+}
